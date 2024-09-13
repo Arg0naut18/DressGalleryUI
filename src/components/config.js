dd30d601581440b3650057fc3c6a6c23e7c87ff3
Arg0naut18 <gaming.genos1729@gmail.com>
2024-09-13 22:55:25 +0530

    Changed Port

--- src/components/config.js
+++ src/components/config.js
@@ -1,6 +1,6 @@
 const config = {
     'host': process.env.REACT_APP_BACKEND_HOST || 'http://0.0.0.0',
-    'port': process.env.REACT_APP_BACKEND_PORT || '8000'
+    'port': process.env.REACT_APP_BACKEND_PORT || '8090'
 }
 
 export default config;
